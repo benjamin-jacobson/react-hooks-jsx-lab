aa3c948b48eb223c70af6c557a76e735e87cae27
BenjaminJacobson <ben.jacobson@flatironschool.com>
2023-09-01 20:26:50 -0400

    compelted lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,12 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const divStyle = {color:'firebrick'}
+  return (
+  <div id="home">
+    <h1 style={divStyle}>Your { name } is a Web Developer from Your { city }</h1>
+  </div>
+  );
 }
 
 export default Home;
